b312f1b0d5f764fe253dd1c9a6afc807b9e204f6
Razvan Smarandeanu <razvan.smarandeanu@erepublik.com>
2014-03-27 12:12:51 +0200

    frontend reconnect

--- client2.js
+++ client2.js
@@ -3,9 +3,10 @@ var clickMilliseconds = 0;
 var matchEnded = false;
 var matchBeggins = false;
 
-var cookieValue = $.cookie("login");
 
 var distractColors = ['#4FFFE2', '#FFC46B', '#DA5EFF'];
+
+var room_name = '';
 
 $(document).ready(function() {
     var socket = io.connect("127.0.0.1:8000");
@@ -22,16 +23,16 @@ $(document).ready(function() {
 
     $("#join").click(function(){
       var name = $("#name").val();
-      var room_name = $("#room_name").val();
+      room_name = $("#room_name").val();
       if (room_name != "") {
 
-      var joinData = {
-        roomName: room_name,
-        side: 0,
-        division: 0,
-        name: 'observer',
-        avatar: 'http://www.erepublik.com/images/default_avatars/Citizens/default_male_55x55.gif',
-      };
+        var joinData = {
+          roomName: room_name,
+          side: 0,
+          division: 0,
+          name: 'observer',
+          avatar: 'http://www.erepublik.com/images/default_avatars/Citizens/default_male_55x55.gif',
+        };
 
         socket.emit("joinserver", joinData);
         $("#login").detach();
@@ -39,7 +40,6 @@ $(document).ready(function() {
         $("#msg").focus();
         ready = true;
 
-        $.cookie("login", name);
       }
     });
 
@@ -80,17 +80,33 @@ $(document).ready(function() {
 
 
     socket.on("disconnect", function(){
+      console.log('disconnect');
       // $("#msgs").append("<li><strong><span class='text-warning'>The server is not available</span></strong></li>");
-      $("#msg").attr("disabled", "disabled");
-      $("#send").attr("disabled", "disabled");
+      // $("#msg").attr("disabled", "disabled");
+      // $("#send").attr("disabled", "disabled");
     });
 
+
+    socket.on("reconnect", function() {
+      console.log('reconnected to the server'); 
+
+        var joinData = {
+          roomName: room_name,
+          side: 0,
+          division: 0,
+          name: 'observer',
+          avatar: 'http://www.erepublik.com/images/default_avatars/Citizens/default_male_55x55.gif',
+        };
+
+        socket.emit("joinserver", joinData);      
+      });
 
 
   if(getParameterByName('room')) {
 
+        room_name = getParameterByName('room')
         var joinData = {
-          roomName: getParameterByName('room'),
+          roomName: room_name,
           side: 0,
           division: 0,
           name: 'observer',
